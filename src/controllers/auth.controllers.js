168ce86c08b21fc780826446b98f27e3e6e81a73
rohit <rohitsirsat855@gmail.com>
2025-05-01 11:00:05 +0530

    add ftn to generate access, refresh toknes and fix the typo

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,155 @@
+import { asyncHandler } from "../utils/async.handler.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { User } from "../models/user.models.js";
+import { UserRolesEnum } from "../utils/constants.js";
+import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js";
+
+const generateAccessTokenAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating the tokens");
+  }
+};
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const user = await User.create({
+    email,
+    username,
+    password,
+    isEmailVerified: false,
+    role: role || UserRolesEnum.MEMBER,
+  });
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationTokenExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Please verify your email",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get(
+        "host",
+      )}/api/v1/verify-email/${unHashedToken}`,
+    ),
+  });
+
+  const { accessToken, refreshToken } =
+    await generateAccessTokenAndRefreshTokens(user._id);
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationTokenExpiry",
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  const cookieOptions = {
+    httpOnly: true, // Set to false for testing
+    secure: true, // Set to false for local development
+  };
+
+  return res
+    .status(201)
+    .cookie("accessToken", accessToken, cookieOptions)
+    .cookie("refreshToken", refreshToken, cookieOptions)
+    .json(
+      new ApiResponse(
+        200,
+        { user: createdUser, accessToken, refreshToken },
+        "User Registered Successfully",
+      ),
+    );
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const resendEmailVerification = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+const resetForgottenPassword = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+export {
+  changeCurrentPassword,
+  forgotPasswordRequest,
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  resendEmailVerification,
+  resetForgottenPassword,
+  verifyEmail,
+};
