cfe6ee0aa5dc6aee2606d53effd2a66cdfbef406
rohit <rohitsirsat855@gmail.com>
2025-09-06 13:48:54 +0530

    add: more note validators

--- src/validators/notes/note.validators.js
+++ src/validators/notes/note.validators.js
@@ -1,7 +1,7 @@
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { AvailableUserRoles } from "../../utils/constants.js";
 
-export const createNoteValidator = () => {
+const createNoteValidator = () => {
   return [
     body("title").trim().notEmpty().withMessage("Title is required"),
     body("description")
@@ -12,7 +12,7 @@ export const createNoteValidator = () => {
   ];
 };
 
-export const updateNoteValidator = () => {
+const updateNoteValidator = () => {
   return [
     body("title").trim().notEmpty().withMessage("Title is required"),
     body("description")
@@ -21,4 +21,45 @@ export const updateNoteValidator = () => {
       .withMessage("Description is required"),
     body("role").isIn(AvailableUserRoles).withMessage("Invalid user role"),
   ];
+};
+
+const getAllNotesValidator = () => {
+  return [
+    param("projectId")
+      .trim()
+      .notEmpty()
+      .withMessage("Project ID is required")
+      .isMongoId()
+      .withMessage("Invalid project ID"),
+  ];
+};
+
+const getNoteByIdValidator = () => {
+  return [
+    param("noteId")
+      .trim()
+      .notEmpty()
+      .withMessage("Note ID is required")
+      .isMongoId()
+      .withMessage("Invalid note ID"),
+  ];
+};
+
+const deleteNoteByIdValidator = () => {
+  return [
+    param("noteId")
+      .trim()
+      .notEmpty()
+      .withMessage("Note ID is required")
+      .isMongoId()
+      .withMessage("Invalid note ID"),
+  ];
+};
+
+export {
+  createNoteValidator,
+  updateNoteValidator,
+  getAllNotesValidator,
+  getNoteByIdValidator,
+  deleteNoteByIdValidator,
 };
