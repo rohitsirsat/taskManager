624df20ed10e11f2d5769957624a945129a8ddb7
rohit <rohitsirsat855@gmail.com>
2025-09-08 20:00:39 +0530

    add: create route

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,5 +1,24 @@
 import { Router } from "express";
+import { createTask } from "../controllers/task.controllers.js";
+import {
+  authCheck,
+  validateProjectPermission,
+} from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { UserRolesEnum } from "../utils/constants.js";
 
 const router = Router();
+
+router
+  .route("/project/:projectId/create-task/:userId")
+  .post(
+    authCheck,
+    validateProjectPermission([
+      UserRolesEnum.ADMIN,
+      UserRolesEnum.PROJECT_ADMIN,
+    ]),
+    upload.array("attachments", 3),
+    createTask,
+  );
 
 export default router;
