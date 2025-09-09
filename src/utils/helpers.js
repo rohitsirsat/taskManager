eb9003dbe67a0adf43c12394d94592e01c40460b
rohit <rohitsirsat855@gmail.com>
2025-09-09 22:40:30 +0530

    add: implement static file path utilities

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13,3 +13,11 @@ export const getMongoosePaginationOptions = ({
     },
   };
 };
+
+export const getStaticFilePath = (req, fileName) => {
+  return `${req.protocol}://${req.get("host")}/images/${fileName}`;
+};
+
+export const getLocalPath = (fileName) => {
+  return `public/images/${fileName}`;
+};
