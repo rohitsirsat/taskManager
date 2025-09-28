7054386874b8df2b8aa8d8252c041c72a78a3933
rohit <rohitsirsat855@gmail.com>
2025-09-28 18:01:49 +0530

    add: server port, cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "localhost",
+    port: 8000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
