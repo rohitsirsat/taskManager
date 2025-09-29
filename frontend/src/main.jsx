fa69a0dc72cd2e32237d6995a11298068b19919f
rohit <rohitsirsat855@gmail.com>
2025-09-30 00:44:57 +0530

    add: position attri

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,7 +18,7 @@ createRoot(document.getElementById("root")).render(
           disableTransitionOnChange
         >
           <App />
-          <Toaster richColors />
+          <Toaster richColors position="top-center" />
         </ThemeProvider>
       </AuthProvider>
     </BrowserRouter>
