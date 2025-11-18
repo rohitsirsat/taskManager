54f3ceeb35a89a9d1e9c40e8922333c6dbba6e86
rohit <rohitsirsat855@gmail.com>
2025-11-18 23:40:29 +0530

    fix: imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,6 @@ import PublicRoute from "./components/PublicRoute.jsx";
 import { useAuth } from "./context/authContex.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
-import ProjectsPage from "./pages/ProjectsPage.jsx";
 
 function App() {
   const { token, user } = useAuth();
